f050de7379612dd767df07a457e3e2bf2263d44f
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2021-07-22 10:16:57 +0300

    Added routing.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './task/task-list.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'users', component: UserListComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TaskModule } from './task/task.module';
+import { AppRoutingModule } from './app-routing.module';
+import { UserListComponent } from './user/user-list/user-list.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    TaskModule
+    TaskModule,
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
